ea5ad10a94743b66e475aa481fd56801bd663e9e
Ivan Ganatra <ivan.ganatra@gmail.com>
2021-06-07 22:20:36 +0530

    Added counter variable

--- to-do-list/src/App.js
+++ to-do-list/src/App.js
@@ -6,12 +6,15 @@ import Welcome from './Components/Welcome.js'
 import  Reviews from './Components/Reviews.js'
 import ToDoList from './Components/ToDoListCount.js'
 import MainPage from './Components/MainPage.js'
+import Parent from './Components/Parent.js'
 function App() {
   return (
     <div className="App">
       <MainPage/>
       <Reviews />
-    </div>
+      <ToDoList />
+      <Parent/>
+      </div>
   );
 }
 
--- to-do-list/src/Components/ToDoListCount.js
+++ to-do-list/src/Components/ToDoListCount.js
@@ -5,30 +5,68 @@ class Counter extends Component {
         super(props)
 
         this.state = {
-             counter:0
+             counter:0,
+             render:false
         }
+        this.fiveincrement=this.fiveincrement.bind(this)
     }
-    increment(){
-        this.setState(
-        (prevState)=>{
-            return {
-            counter:prevState.counter+1
-            };
+    componentDidMount() {
+        setTimeout(function() { //Start the timer
+
+            this.setState({render: true}) //After 1 second, set render to true
+        }.bind(this), 1000)
+    }
+    oneincrement(){
+        this.componentDidMount();
+        console.log(this.state.render);
+        if(this.state.render){
+            this.setState(
+                (prevState)=>{
+                    return {
+                    render:false,
+                    counter:prevState.counter+1
+                };
+            }
+            )
         }
-        )
     }
     fiveincrement(){
-        this.increment()
-        this.increment()
-        this.increment()
-        this.increment()
-        this.increment()
+        console.log(this.state.render);
+        if(this.state.render===false){
+            this.componentDidMount();
+            console.log(this.state.render);
+            if(this.state.render){
+                this.setState(
+                    (prevState)=>{
+                        return {
+                        render:false,
+                        counter:prevState.counter+5
+                    };
+                }
+                )
+            }
+        }
     }
+    increment(loops){
+        let loop=loops
+        console.log("loop"+loops)
+        while(loop-5>=0)
+        {
+            this.fiveincrement();
+            loop-=5;
+        }
+        while(loop>0)
+        {
+            this.increment();
+            loop-=1;
+        }
+    }
+
     render() {
         return (
             <div className="ToDoList">
                 <h2>Number of TodoList Created On This App</h2> -<h1>{this.state.counter}</h1>
-                <button onClick={()=>{this.fiveincrement()}}>Increment</button>
+                <button onClick={()=>{this.increment(100)}}>Increment</button>
             </div>
         )
     }
